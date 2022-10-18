dcd26074b79ef10bc477ebc3548ff176c29bd2e6
mmf09 <mmf09@YPC>
2022-10-17 21:41:35 -0700

    Add project files.

---
+++ Car Insurance Approval/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Car_Insurance_Approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had DUIs? Please answer with \"True\" or \"False\" only.");
+            bool duis = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualifications = (age > 15) && (!duis) && (tickets <= 3);
+            Console.WriteLine("Qualified: " + qualifications);
+
+
+            Console.Read();
+        }
+    }
+}
